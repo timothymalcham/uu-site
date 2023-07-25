92da904609060adb4cb69eb85e10566bb61f5017
Timothy Malcham <timothymalcham@gmail.com>
2023-07-25 12:04:25 +0100

    lots of design fixes

---
+++ src/content/posts/01-the-power-of-react-hooks copy.md
@@ -0,0 +1,13 @@
+---
+title: "Welcome!"
+publishedAt: 2023-07-25
+description: "The very first post."
+slug: "the-very-first-posts"
+isPublish: true
+---
+
+## Welcome
+
+This site is mostly still under construction. Check back soon for more! 
+
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,13 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Bluu Next", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
