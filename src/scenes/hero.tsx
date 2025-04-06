03f915d5e17743771d7af1969862e0b93e232d54
Timothy Malcham <timothymalcham@gmail.com>
2025-04-06 17:59:02 +0100

    feat: more blobs

--- src/scenes/hero.tsx
+++ src/scenes/hero.tsx
@@ -30,9 +30,30 @@ export function Hero() {
                         <a.ambientLight intensity={ambient} />
                         <a.pointLight ref={light} position-z={-15} intensity={env} color="#F8C069" />
                     </PerspectiveCamera>
-                    <mesh ref={sphere}>
+
+                    <mesh ref={sphere} position={[-5, 2, -4]}>
                         <sphereGeometry args={[1, 64, 64]} />
-                        <AnimatedMaterial distort={0.7} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
+                        <AnimatedMaterial distort={0.6} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
+                    </mesh>
+
+                    <mesh position={[-2, -1.25, 0.15]}>
+                        <sphereGeometry args={[1, 64, 64]} />
+                        <AnimatedMaterial distort={0.6} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
+                    </mesh>
+
+                    <mesh position={[0, 1, 0.85]}>
+                        <sphereGeometry args={[1, 64, 64]} />
+                        <AnimatedMaterial distort={0.6} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
+                    </mesh>
+
+                    <mesh position={[2, -1.25, 0]}>
+                        <sphereGeometry args={[1, 64, 64]} />
+                        <AnimatedMaterial distort={0.6} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
+                    </mesh>
+
+                    <mesh position={[4, 2, -0.5]}>
+                        <sphereGeometry args={[1, 64, 64]} />
+                        <AnimatedMaterial distort={0.5} speed={1} color={color} envMapIntensity={env} clearcoat={coat} clearcoatRoughness={0.1} metalness={0.05} />
                     </mesh>
                 </group>
                 <Environment preset="warehouse" />
